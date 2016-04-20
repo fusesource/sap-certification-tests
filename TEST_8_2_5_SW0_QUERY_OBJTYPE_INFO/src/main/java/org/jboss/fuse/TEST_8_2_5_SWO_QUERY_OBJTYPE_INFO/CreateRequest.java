e19a9f6f7bf65287566b4a41df06cd5ead2b508d
William Collins <punkhornsw@gmail.com>
2016-04-20 15:52:57 -0400

    Added TEST_8_2_5_SWO_QUERY_OBJTYPE_INFO certification test

---
+++ TEST_8_2_5_SW0_QUERY_OBJTYPE_INFO/src/main/java/org/jboss/fuse/TEST_8_2_5_SWO_QUERY_OBJTYPE_INFO/CreateRequest.java
@@ -0,0 +1,21 @@
+package org.jboss.fuse.TEST_8_2_5_SWO_QUERY_OBJTYPE_INFO;
+
+import org.apache.camel.Exchange;
+import org.fusesource.camel.component.sap.SapSynchronousRfcDestinationEndpoint;
+import org.fusesource.camel.component.sap.model.rfc.Structure;
+
+public class CreateRequest {
+
+	public void createRequest(Exchange exchange) throws Exception {
+
+		SapSynchronousRfcDestinationEndpoint endpoint = exchange.getContext().getEndpoint("sap-srfc-destination:quickstartDest:SWO_QUERY_OBJTYPE_INFO", SapSynchronousRfcDestinationEndpoint.class);
+		Structure request = endpoint.createRequest();
+		
+		request.put("OBJTYPE", "BUS1001");
+		request.put("TEXT", "X");
+		request.put("LANGUAGE", "EN");
+		
+		exchange.getIn().setBody(request);
+	}
+
+}
